bbd0c26af5bc05208dc8b7e559438097b7b8d1b3
Mahesh <mahesh.yemmanuru@salesforce.com>
2018-11-17 20:26:46 -0800

    Asteroid Collision

--- src/main/java/AlmostPalindrome.java
+++ src/main/java/AlmostPalindrome.java
@@ -25,6 +25,7 @@ public class AlmostPalindrome {
 
 
     private static boolean isPalindrome(String str){
+        System.out.println("str: "+str);
         for(int i=0;i<str.length()/2;i++){
             if(str.charAt(i)!=str.charAt(str.length()-i-1)){
                 return false;
